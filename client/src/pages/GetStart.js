a9c12e702facef2022a1a0064fb1e00bcf90104e
Ryan Xu <Ryan.Xu@TRULIOO.NET>
2024-04-23 17:47:04 -0700

    wip

---
+++ client/src/pages/GetStart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// @ts-ignore
+import joinMediSync from './startNow/images/join-medisync.svg';
+
+const GetStart = () => {
+  return (
+    <>
+      <img src={joinMediSync} alt="Join Medisync!" />
+      <div></div>
+    </>
+  );
+};
+
+export default GetStart;
+
